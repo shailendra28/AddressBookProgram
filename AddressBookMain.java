14df8450a4952a0128604d195b3ea1b33befaeda
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-09 20:47:14 +0530

    ADDED: Contact Detail In Address Book Program

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,37 @@
+class contactDetail {
+    public final String firstname, lastname;
+    public final String address, city, state;
+    public final String zip;
+    public final String phoneNumber;
+    public final String email;
+
+    public contactDetail(String firstname, String lastname, String address, String city, String state, String zip, String phonenumber, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phonenumber;
+        this.email = email;
+    }
+    public void printContactDetails() {
+    System.out.println("Firstname : " + firstname);
+    System.out.println("Lastname : " + lastname);
+    System.out.println("Address : " + address);
+    System.out.println("City : " + city);
+    System.out.println("State : " + state);
+    System.out.println("Zip : " + zip);
+    System.out.println("PhoneNumber : " + phoneNumber);
+    System.out.println("Email : " + email);
+    }
+}
 
 public class AddressBookMain {
+    public static void main(String args[]) {
+        System.out.println("Wellcome to Address Book Program");
 
-	public static void main(String[] args) {
-		System.out.println("Wellcome to Address Book Program");
-
-	}
-
+        contactDetail contact = new contactDetail("Shailendra", "Yadav", "Chembur", "Mumbai", "Maharashtra", "400071", "8779796556", "yshailendrakumar1768@gmail.com");
+        contact.printContactDetails();
+        }
 }
