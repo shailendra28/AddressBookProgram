bc68b3c208e69049206ea366e0530130af6f720c
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-10 02:18:06 +0530

    ADDED: Ability To Add Edit Existing Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -74,37 +74,110 @@ class addressBook {
 		public void insertContact(contactDetail contactDetailObject) {
 			contactList.add(contactDetailObject);
 		}
-}
+
+		public void editContact(String nameToEdit) {
+	        for(contactDetail editContact: contactList) {
+				if(editContact.getFirstName().equals(nameToEdit)) {
+		        	
+					System.out.println("Edit Firstname : ");
+		        	String firstname = sc.nextLine();
+		        	editContact.setFirstName(firstname);
+		        	
+					System.out.println("Edit Lastname : ");
+		        	String lastname = sc.nextLine();
+					editContact.setLastName(lastname);
+	                
+					System.out.println("Edit Address : ");
+	                String address = sc.nextLine();
+					editContact.setAddress(address);
+	                
+					System.out.println("Edit City : ");
+	                String city = sc.nextLine();
+					editContact.setCity(city);
+	                
+					System.out.println("Edit State : ");
+	                String state = sc.nextLine();
+					editContact.setState(state);
+	                
+					System.out.println("Edit Zip : ");
+	                int zip = sc.nextInt();
+					editContact.setZip(zip);
+	                
+					System.out.println("Edit PhoneNumber : ");
+	                long phonenumber = sc.nextLong();
+					scan.nextLine();
+					editContact.setPhoneNo(phonenumber);
+	                
+					System.out.println("Edit Email : ");
+	                String email = sc.nextLine();
+					editContact.setEmail(email);
+	            }
+	        }
+	    }
+		 public void printContactDetails() {
+		        for(contactDetail getInfo: contactList) {
+		            System.out.println("Firstname : " + getInfo.getFirstName());
+		            System.out.println("Lastname : " + getInfo.getLastName());
+		            System.out.println("Address : " + getInfo.getAddress());
+		            System.out.println("City : " + getInfo.getCity());
+		            System.out.println("State : " + getInfo.getState());
+		            System.out.println("Zip : " + getInfo.getZip());
+		            System.out.println("PhoneNumber : " + getInfo.getPhoneNo());
+		            System.out.println("Email : " + getInfo.getEmail());
+		        }
+		    }
+
+		}
 public class AddressBookMain {
     public static void main(String args[]) {
     	Scanner scan = new Scanner(System.in);
-		 addressBook addressObject = new addressBook();
+		addressBook addressObject = new addressBook();
 		String choice = "y";
-       System.out.println("Wellcome to Address Book Program!");
+		System.out.println("Wellcome to Address Book Program!");
+		while(true) {
+			System.out.println("Enter 1: add 2: edit 3: exit");
+			System.out.println("Enter choice : ");
+			int switchChoice = scan.nextInt();
+			scan.nextLine();
+			
+			switch(switchChoice) {
+				case 1:
+					while(choice.equals("y")) {
+						System.out.println("Firstname : ");
+ 						String firstname = scan.nextLine();
+						System.out.println("Lastname : ");
+						String lastname = scan.nextLine();
+						System.out.println("Address : ");
+						String address = scan.nextLine();
+						System.out.println("City : ");
+						String city = scan.nextLine();
+						System.out.println("State : ");
+						String state = scan.nextLine();
+						System.out.println("Zip : ");
+						int zip = scan.nextInt();
+						System.out.println("PhoneNumber : ");
+						long phonenumber = scan.nextLong();
+						System.out.println("Email : ");
+						scan.nextLine();
+						String email = scan.nextLine();
+						System.out.println("Do you want add more contact : ");
+						choice = scan.nextLine();
 
-		while(choice.equals("y") || choice.equals("Y")) {
-	    	System.out.println("Firstname : ");
-			String firstname = scan.nextLine();
-	    	System.out.println("Lastname : ");
-			String lastname = scan.nextLine();
-	    	System.out.println("Address : ");
-			String address = scan.nextLine();
-	    	System.out.println("City : ");
-			String city = scan.nextLine();
-	    	System.out.println("State : ");
-			String state = scan.nextLine();
-	    	System.out.println("Zip : ");
-			int zip = scan.nextInt();
-	    	System.out.println("PhoneNumber : ");
-			long phonenumber = scan.nextLong();
-	    	System.out.println("Email : ");
-			String email = scan.nextLine();
-			System.out.println("Do you want add more contact : ");
-			choice = scan.nextLine();
-			scan.close();
-			contactDetail contact = new contactDetail(firstname, lastname, address, city, state, zip, phonenumber, email);
-			addressObject.insertContact(contact);
+						contactDetail contact = new contactDetail(firstname, lastname, address, city, state, zip, phonenumber, email);
+						addressObject.insertContact(contact);
+					}
+				break;
+				case 2:
+					System.out.println("Enter name of contact to edit : ");
+					String nameToEdit = scan.nextLine();
+					addressObject.editContact(nameToEdit);
+					System.out.println("Contact List : ");
+					addressObject.printContactDetails();
+				break;
+				default:
+					System.exit(0);
+			}
 		}
 
-   }
+	}
 }
